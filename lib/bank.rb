d473aaf21533173e8dd1d32a2e2c157fdcc9624e
Teako Warfield-Graham <teakowg@gmail.com>
2017-01-23 15:19:39 +0000

    Bank statement prints with basic format

--- lib/bank.rb
+++ lib/bank.rb
@@ -7,16 +7,28 @@ class Bank
     @statement = []
   end
 
-  def deposit(money, date)
+  def deposit(money)
     date = Date.today.to_s
     @balance += money
-    transaction = { transaction_date: date, deposit: money }
+    transaction = { transaction_date: date, credit: money, debit: 0, balance: @balance }
     @statement << transaction
   end
 
-  def withdraw(money, date)
+  def withdraw(money)
+    date = Date.today.to_s
     @balance -= money
-    @statement << @balance
+    transaction = { transaction_date: date, credit: 0, debit: money, balance: @balance }
+    @statement << transaction
   end
+
+  def format_data
+    @statement.collect {|data| "#{data[:transaction_date]} | #{data[:credit]} | #{data[:debit]} | #{data[:balance]}"}
+  end
+
+  def print_statement
+    puts "Date       | Credit | Debit | Balance |"
+    puts format_data
+  end
+
 
 end
--- spec/bank_spec.rb
+++ spec/bank_spec.rb
@@ -6,7 +6,7 @@ describe Bank do
   let(:date) { double(:date) }
 
   before do
-    bank.deposit(100, date)
+    bank.deposit(100)
   end
 
   context "Keeps track of the user's balance and transaction dates" do
@@ -16,12 +16,21 @@ describe Bank do
     end
 
     it 'allows a user to withdraw money' do
-      bank.withdraw(50, date)
+      bank.withdraw(50)
       expect(bank.balance).to eq(50)
     end
 
     it 'assigns dates to each user deposit' do
-      expect(bank.statement).to eq([{ transaction_date: "2017-01-23", deposit: 100 }])
+      expect(bank.statement).to eq([{ transaction_date: "2017-01-23", credit: 100, debit: 0, balance: 100 }])
+    end
+
+    it 'assigns dates to each user withdrawl' do
+      bank.withdraw(50)
+      expect(bank.statement).to include({ transaction_date: "2017-01-23", credit: 0, debit: 50, balance: 50 })
+    end
+
+    it 'has a statement with credits, debits, dates, and balance' do
+      expect(bank.print_statement).to include("date, credit, debit, balance")
     end
 
   end
